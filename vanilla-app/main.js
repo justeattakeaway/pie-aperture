942c977a1b68f28dbd5ea57f956c641793e267bc
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-09-07 16:00:24 +0100

    add all components to vanilla app

--- vanilla-app/main.js
+++ vanilla-app/main.js
@@ -1,7 +1,11 @@
 import '@justeattakeaway/pie-css';
 import '@justeattakeaway/pie-button';
 import '@justeattakeaway/pie-modal';
+import '@justeattakeaway/pie-divider';
 import '@justeattakeaway/pie-icon-button';
+import '@justeattakeaway/pie-cookie-banner';
+import '@justeattakeaway/pie-card-container';
+import '@justeattakeaway/pie-toggle-switch';
 import '@justeattakeaway/pie-icons-webc/dist/icons/IconClose';
 import '@justeattakeaway/pie-icons-webc/dist/icons/IconSearch';
 import './style.css';
@@ -14,8 +18,12 @@ document.querySelector('#app').innerHTML = `
         Modal content
     </pie-modal>
 
+    <pie-divider></pie-divider>
+
     <h2>pie-button Component Counter</h2>
     <pie-button id="counter" type="button"></pie-button>
+
+    <pie-divider></pie-divider>
 
     <h2>pie-icon-button Component – trailing and leading icons</h2>
     <pie-button>
@@ -24,8 +32,24 @@ document.querySelector('#app').innerHTML = `
         <icon-close slot="icon-trailing"></icon-close>
     </pie-button>
 
+    <pie-divider></pie-divider>
+
     <h2>pie-icon-button Component</h2>
     <pie-icon-button size="medium" type="button" variant="primary"><icon-close></icon-close></pie-icon-button>
+
+    <pie-divider></pie-divider>
+    <pie-cookie-banner></pie-cookie-banner>
+
+    <h2>pie-card-container</h2>
+    <pie-card-container>
+        <h2>pie-card</h2>
+        <p>Some content</p>
+    </pie-card-container>
+
+    <pie-divider></pie-divider>
+
+    <h2>pie-toggle-switch</h2>
+    <pie-toggle-switch></pie-toggle-switch>
 `;
 
 setupCounter(document.querySelector('#counter'));
