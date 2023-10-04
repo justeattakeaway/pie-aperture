f6302124ae4f3a8b5ea87992e31017206e356c40
jamieomaguire <jamieolivermaguire@googlemail.com>
2023-10-04 10:39:04 +0100

    tidy code structure

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@justeattakeaway/pie-css';
 import '@/styles/main.scss';
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import Layout from '../layouts/default'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
