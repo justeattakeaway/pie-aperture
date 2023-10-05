e85c56608be3ab28e3317b9550366787875bb751
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-10-05 14:59:53 +0100

    fix: DSW-0 fix next build with temp @ts-ignore

--- nextjs-app/src/components/layout.tsx
+++ nextjs-app/src/components/layout.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
+// @ts-ignore
 export default function Layout({ children }) {
   return (
     <>
