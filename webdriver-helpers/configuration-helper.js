9b32116662b5b90b94dd5415bc25c9fec3f80598
Ben Siggery <ben.siggery@justeattakeaway.com>
2023-10-24 10:07:24 +0100

    ci: DSW-1245 ability to run tests locally + conditional CI path trigger

--- webdriver-helpers/configuration-helper.js
+++ webdriver-helpers/configuration-helper.js
@@ -1,14 +1,23 @@
-const { PR_NUMBER, VANILLA_AMPLIFY_ID, NEXT_AMPLIFY_ID } = process.env;
+const { CI, GITHUB_REF_NAME, PR_NUMBER, VANILLA_AMPLIFY_ID, NEXT_AMPLIFY_ID } = process.env;
 
 exports.getEnvironmentBaseUrl = (appName) => {
-
-    let amplifyId;
 
     if (appName !== 'vanilla-app' && appName !== 'nextjs-app') {
         throw new Error(`appName: '${appName}' is invalid. Please use either 'vanilla-app' or 'nextjs-app'.`);
     }
 
-    amplifyId = appName === 'vanilla-app' ? VANILLA_AMPLIFY_ID : NEXT_AMPLIFY_ID;
+    if (CI) {
+        const amplifyId = appName === 'vanilla-app' ? VANILLA_AMPLIFY_ID : NEXT_AMPLIFY_ID;
 
-    return `https://pr${PR_NUMBER}.${amplifyId}.amplifyapp.com`;
+        if (GITHUB_REF_NAME === 'main') {
+            return `https://main.${amplifyId}.amplifyapp.com`;
+        }
+
+        return `https://pr${PR_NUMBER}.${amplifyId}.amplifyapp.com`;
+    }
+    else {
+        const port = appName === 'vanilla-app' ? '5173' : '3000';
+
+        return `http://localhost:${port}`;
+    }
 }
