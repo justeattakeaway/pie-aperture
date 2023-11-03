50786591a731b0c35789c71cb2a8092ceb7359c6
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-11-03 11:43:23 +0000

    Update to use latest component versions

--- vanilla-app/main.js
+++ vanilla-app/main.js
@@ -1,20 +1,24 @@
 import '@justeattakeaway/pie-css';
 import '@justeattakeaway/pie-button';
-import '@justeattakeaway/pie-modal';
+import '@justeattakeaway/pie-card';
+import '@justeattakeaway/pie-cookie-banner';
 import '@justeattakeaway/pie-divider';
 import '@justeattakeaway/pie-icon-button';
-import '@justeattakeaway/pie-cookie-banner';
-import '@justeattakeaway/pie-card-container';
-import '@justeattakeaway/pie-toggle-switch';
-import '@justeattakeaway/pie-icons-webc/dist/icons/IconClose';
-import '@justeattakeaway/pie-icons-webc/dist/icons/IconSearch';
+import '@justeattakeaway/pie-modal';
+import '@justeattakeaway/pie-switch';
+import '@justeattakeaway/pie-icons-webc/IconClose';
+import '@justeattakeaway/pie-icons-webc/IconSearch';
 import './style.css';
 import { setupCounter } from './counter';
 
 document.querySelector('#app').innerHTML = `
     <h2>pie-modal</h2>
     <pie-button id="modal-trigger" type="button">open modal</pie-button>
-    <pie-modal id="modal" heading='My Awesome Heading' headingLevel='h3' isDismissible>
+    <pie-modal
+        id="modal"
+        heading="My Awesome Heading"
+        isDismissible
+        leadingAction='${JSON.stringify({ text: 'Example' })}'>
         Modal content
     </pie-modal>
 
@@ -27,29 +31,31 @@ document.querySelector('#app').innerHTML = `
 
     <h2>pie-icon-button Component – trailing and leading icons</h2>
     <pie-button>
-        <icon-search slot="icon-leading"></icon-search>
+        <icon-search slot="icon"></icon-search>
         Search
-        <icon-close slot="icon-trailing"></icon-close>
     </pie-button>
 
     <pie-divider></pie-divider>
 
     <h2>pie-icon-button Component</h2>
-    <pie-icon-button size="medium" type="button" variant="primary"><icon-close></icon-close></pie-icon-button>
+    <pie-icon-button size="medium" type="button" variant="primary">
+        <icon-close></icon-close>
+    </pie-icon-button>
 
     <pie-divider></pie-divider>
+
     <pie-cookie-banner></pie-cookie-banner>
 
-    <h2>pie-card-container</h2>
-    <pie-card-container>
+    <h2>pie-card</h2>
+    <pie-card>
         <h2>pie-card</h2>
         <p>Some content</p>
-    </pie-card-container>
+    </pie-card>
 
     <pie-divider></pie-divider>
 
-    <h2>pie-toggle-switch</h2>
-    <pie-toggle-switch></pie-toggle-switch>
+    <h2>pie-switch</h2>
+    <pie-switch></pie-switch>
 `;
 
 setupCounter(document.querySelector('#counter'));
