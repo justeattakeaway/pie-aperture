f6430b6d5bbf667641d3cb09bf8935a8cfc17af9
Raouf <raouf.sawehli@justeattakeaway.com>
2023-11-22 16:39:12 +0100

    upgrade components to latest lit 3
    fix font imports
    
    fix font imports
    
    fix font imports
    
    fix font imports
    
    fix font imports

--- nextjs-app/src/pages/index.tsx
+++ nextjs-app/src/pages/index.tsx
@@ -1,9 +1,5 @@
-
 import Image from 'next/image'
 import Link from 'next/link'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
