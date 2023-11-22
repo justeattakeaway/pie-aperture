aabd1a11cabdf5459802185cb571e9a836cbfc70
Raouf <abdulraoufswehli58@gmail.com>
2023-11-22 17:13:05 +0100

    upgrade components to use lit@3 (#30)
    * test lit 3
    
    * upgrade components to latest lit 3
    
    fix font imports
    
    fix font imports
    
    fix font imports
    
    fix font imports
    
    fix font imports

--- nextjs-app/src/pages/_document.tsx
+++ nextjs-app/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preload" href="https://d30v2pzvrfyzpo.cloudfront.net/fonts/JETSansDigital-Regular-optimised.woff2" as="font" type="font/woff2"  />
+        <link rel="preload" href="https://d30v2pzvrfyzpo.cloudfront.net/fonts/JETSansDigital-Bold-optimised.woff2" as="font" type="font/woff2" />    
+      </Head>
       <body>
         <Main />
         <NextScript />
