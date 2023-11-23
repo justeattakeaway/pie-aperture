d90108d0d9b1fa2ae7f2d04b593c184b9d2e2dbb
Raouf <raouf.sawehli@justeattakeaway.com>
2023-11-23 16:26:41 +0100

    add missing components to nextjs and align it to the js app setup and layout

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import '@justeattakeaway/pie-css';
-import '@/styles/main.scss';
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "@justeattakeaway/pie-css";
+import "@/styles/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      )
+  return (
+    <>
+      <Head>
+        <title>PIE Aperture</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
