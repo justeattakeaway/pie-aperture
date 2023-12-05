f4e61e49f41a6e5748fb4557aa12ba23b81223bd
personal <jamieolivermaguire@googlemail.com>
2023-12-05 14:53:34 +0000

    configure vanilla icons page build

--- vanilla-app/test/vanilla.spec.js
+++ vanilla-app/test/vanilla.spec.js
@@ -9,12 +9,11 @@ describe('Vanilla App', () => {
         await percyScreenshot('Vanilla App - Component Overview');
     })
 
-    // TODO: Uncomment when icons page is properly set up to be deployed
-    // it('should navigate to the component overview page.', async () => {
-    //     await browser.url('/icons.html');
-    //     await waitUntilPageLoad();
-    //     await expect(await browser.getTitle()).toBe('PIE Aperture | Icons');
-    //     await percyScreenshot('Vanilla App - Icons Page');
-    // })
+    it('should navigate to the component overview page.', async () => {
+        await browser.url('/icons.html');
+        await waitUntilPageLoad();
+        await expect(await browser.getTitle()).toBe('PIE Aperture | Icons');
+        await percyScreenshot('Vanilla App - Icons Page');
+    })
 })
 
