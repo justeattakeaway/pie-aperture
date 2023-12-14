af6aa96fba40a7f069707bf8b7f1eb966df41694
personal <jamieolivermaguire@googlemail.com>
2023-12-14 09:05:20 +0000

    add form test page to vanilla

--- vanilla-app/main.js
+++ vanilla-app/main.js
@@ -1,82 +1,82 @@
 import '@justeattakeaway/pie-css';
 import '@justeattakeaway/pie-button';
-// import '@justeattakeaway/pie-card';
-// import '@justeattakeaway/pie-cookie-banner';
-// import '@justeattakeaway/pie-divider';
-// import '@justeattakeaway/pie-form-label';
-// import '@justeattakeaway/pie-icon-button';
-// import '@justeattakeaway/pie-modal';
+import '@justeattakeaway/pie-card';
+import '@justeattakeaway/pie-cookie-banner';
+import '@justeattakeaway/pie-divider';
+import '@justeattakeaway/pie-form-label';
+import '@justeattakeaway/pie-icon-button';
+import '@justeattakeaway/pie-modal';
 import '@justeattakeaway/pie-switch';
-// import '@justeattakeaway/pie-spinner';
-// import '@justeattakeaway/pie-link';
-// import '@justeattakeaway/pie-icons-webc/IconClose';
-// import '@justeattakeaway/pie-icons-webc/IconSearch';
+import '@justeattakeaway/pie-spinner';
+import '@justeattakeaway/pie-link';
+import '@justeattakeaway/pie-icons-webc/IconClose';
+import '@justeattakeaway/pie-icons-webc/IconSearch';
 
-// import './style.css';
-// import { setupCounter } from './counter';
+import './style.css';
+import { setupCounter } from './counter';
 
-// document.querySelector('#app').innerHTML = `
-//     <h2>pie-modal</h2>
-//     <pie-button id="modal-trigger" type="button">open modal</pie-button>
-//     <pie-modal
-//         id="modal"
-//         heading="My Awesome Heading"
-//         headingLevel="h3"
-//         leadingAction='${JSON.stringify({ text: 'Leading action' })}'
-//         isDismissible>
-//         Modal content
-//     </pie-modal>
+document.querySelector('#app').innerHTML = `
+    <h2>pie-modal</h2>
+    <pie-button id="modal-trigger" type="button">open modal</pie-button>
+    <pie-modal
+        id="modal"
+        heading="My Awesome Heading"
+        headingLevel="h3"
+        leadingAction='${JSON.stringify({ text: 'Leading action' })}'
+        isDismissible>
+        Modal content
+    </pie-modal>
 
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
 
-//     <h2>pie-button Component Counter</h2>
-//     <pie-button id="counter" type="button"></pie-button>
+    <h2>pie-button Component Counter</h2>
+    <pie-button id="counter" type="button"></pie-button>
 
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
 
-//     <h2>pie-button Component – with icon and text</h2>
-//     <pie-button>
-//         <icon-search slot="icon"></icon-search>
-//         Search
-//     </pie-button>
+    <h2>pie-button Component – with icon and text</h2>
+    <pie-button>
+        <icon-search slot="icon"></icon-search>
+        Search
+    </pie-button>
 
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
 
-//     <h2>pie-icon-button</h2>
-//     <pie-icon-button onclick="alert('clicked')"><icon-close></icon-close></pie-icon-button>
+    <h2>pie-icon-button</h2>
+    <pie-icon-button onclick="alert('clicked')"><icon-close></icon-close></pie-icon-button>
 
-//     <pie-divider></pie-divider>
-//     <pie-cookie-banner></pie-cookie-banner>
+    <pie-divider></pie-divider>
+    <pie-cookie-banner></pie-cookie-banner>
 
-//     <h2>pie-card</h2>
-//     <pie-card padding="a">
-//         <h2>pie-card</h2>
-//         <p>Some content</p>
-//     </pie-card>
+    <h2>pie-card</h2>
+    <pie-card padding="a">
+        <h2>pie-card</h2>
+        <p>Some content</p>
+    </pie-card>
 
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
 
-//     <h2>pie-switch</h2>
-//     <pie-switch></pie-switch>
+    <h2>pie-switch</h2>
+    <pie-switch></pie-switch>
 
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
 
-//     <h2>pie-link</h2>
-//     <pie-link href="http://www.pie.design" target="_blank">Visit pie.design</pie-link>
+    <h2>pie-link</h2>
+    <pie-link href="http://www.pie.design" target="_blank">Visit pie.design</pie-link>
 
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
 
-//     <h2>pie-spinner</h2>
-//     <pie-spinner></pie-spinner>
+    <h2>pie-spinner</h2>
+    <pie-spinner></pie-spinner>
     
-//     <pie-divider></pie-divider>
+    <pie-divider></pie-divider>
     
-//     <h2>pie-form-label</h2>
-//     <pie-form-label>Label</pie-form-label>
-// `;
+    <h2>pie-form-label</h2>
+    <pie-form-label>Label</pie-form-label>
+`;
 
-// setupCounter(document.querySelector('#counter'));
+setupCounter(document.querySelector('#counter'));
 
-// document.querySelector('#modal-trigger').addEventListener('click', () => {
-//     document.querySelector('#modal').setAttribute('isOpen', true);
-// });
+document.querySelector('#modal-trigger').addEventListener('click', () => {
+    document.querySelector('#modal').setAttribute('isOpen', true);
+});
--- vanilla-app/test/vanilla.spec.js
+++ vanilla-app/test/vanilla.spec.js
@@ -7,13 +7,20 @@ describe('Vanilla App', () => {
         await waitUntilPageLoad();
         await expect(await browser.getTitle()).toBe('PIE Aperture');
         await percyScreenshot('Vanilla App - Component Overview');
-    })
+    });
 
-    it('should navigate to the component overview page.', async () => {
+    it('should navigate to the icons page.', async () => {
         await browser.url('/icons.html');
         await waitUntilPageLoad();
         await expect(await browser.getTitle()).toBe('PIE Aperture | Icons');
         await percyScreenshot('Vanilla App - Icons Page');
-    })
-})
+    });
+
+    it('should navigate to the form page.', async () => {
+        await browser.url('/form.html');
+        await waitUntilPageLoad();
+        await expect(await browser.getTitle()).toBe('PIE Aperture | Form Integration');
+        await percyScreenshot('Vanilla App - Form Page');
+    });
+});
 
