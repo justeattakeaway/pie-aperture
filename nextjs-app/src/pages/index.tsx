8bf3f57954a7dd83ffdadae12e5665c0c187e80b
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-01-25 16:54:01 +0000

    PR cleanup

--- nextjs-app/src/pages/index.tsx
+++ nextjs-app/src/pages/index.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <>
       <h2>Icon Close</h2>
-      <IconClose size='xs'></IconClose>
+      <IconClose></IconClose>
 
       <hr />
 
       <h2>Pie Icon Button</h2>
       <PieIconButton onClick={() => alert('clicked')}>
-        <IconClose size='xs'></IconClose>
+        <IconClose></IconClose>
       </PieIconButton>
     </>
   );
