3564b4b57555a5fb9df5cc3fcdd6dd09dbe18988
Fernando de França <448801+fernandofranca@users.noreply.github.com>
2024-01-30 14:33:29 +0100

    chore(pie-aperture): DSW-1671 fix TS errors

--- nextjs-app/src/pages/modal-cookie-banner.tsx
+++ nextjs-app/src/pages/modal-cookie-banner.tsx
@@ -1,6 +1,8 @@
 import * as icons from '@justeattakeaway/pie-icons-webc/dist/react';
 import { useState } from 'react';
 import { PieModal } from '@justeattakeaway/pie-modal/dist/react';
+// @ts-ignore: missing declaration for locales
+import locales from "@justeattakeaway/pie-cookie-banner/locales"
 import { PieCookieBanner } from "@justeattakeaway/pie-cookie-banner/dist/react"
 
 export default function Icons() {
@@ -28,7 +30,11 @@ export default function Icons() {
                 isDismissible hasBackButton>
                 <p>This is the modal content!</p>
             </PieModal>
-            <PieCookieBanner />
+            <PieCookieBanner
+                locale={locales.enGB}
+                hasPrimaryActionsOnly={true}
+                cookieTechnologiesLink="https://justeattakeaway.com"
+                cookieStatementLink="https://justeattakeaway.com" />
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
