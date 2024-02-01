e931a495f26e68193da3e05528fcc30ba8476787
Ben Siggery <bensiggery28@gmail.com>
2024-02-01 10:41:45 +0000

    chore: DSW-0 remove ssg

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -4,11 +4,6 @@ const withLitSSR = require('@lit-labs/nextjs')();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // This makes nextjs build via SSG as this is currently the simplist way to deploy to AWS Amplify.
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
 }
 
 module.exports = withLitSSR(nextConfig);
