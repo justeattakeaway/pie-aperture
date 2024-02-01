16808ba4942a72a24a29621d8bf5afccb00466d5
Ben Siggery <bensiggery28@gmail.com>
2024-02-01 14:53:05 +0000

    chore: DSW-0 ignore build errors

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -4,6 +4,15 @@ const withLitSSR = require('@lit-labs/nextjs')();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    //
+    // We enable this only in Amplify as it seems to add ~5mins to build time. There shouldn't be as risk with doing this, as we'll still capture issues for any builds executed as part of GitHub Actions (such as the other OS / Node builds. )
+    // !! WARN !!
+    ignoreBuildErrors: process.env.AMPLIFY_MONOREPO_APP_ROOT ? true : false,
+  }
 }
 
 module.exports = withLitSSR(nextConfig);
