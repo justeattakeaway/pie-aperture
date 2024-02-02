cb07c0f31ca420f0fd4d3d867f340e29d981db57
personal <jamieolivermaguire@googlemail.com>
2024-02-02 16:00:55 +0000

    workspaces

---
+++ nuxt-app/test/system/form.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Form Page', () => {
+    test('should submit the correct form data', async ({ page }) => {
+        // page.goto('/form');
+    });
+});
