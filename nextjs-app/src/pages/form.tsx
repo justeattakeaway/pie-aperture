16c338a96aa98a2c09ad18d01e1b91ce900ba617
Ben Siggery <bensiggery28@gmail.com>
2024-02-07 14:06:29 +0000

    chore: DSW-0 remove refactors

--- nextjs-app/src/pages/form.tsx
+++ nextjs-app/src/pages/form.tsx
@@ -58,11 +58,7 @@ export default function Form() {
                     Username:
                 </label>
                 <input
-<<<<<<< HEAD
                     className="form-field"
-=======
-                    className="form__field"
->>>>>>> fa6dec8 (update vanilla and nextjs forms)
                     id="username"
                     name="username"
                     value={username}
@@ -73,11 +69,7 @@ export default function Form() {
                     Email:
                 </label>
                 <input
-<<<<<<< HEAD
                     className="form-field"
-=======
-                    className="form__field"
->>>>>>> fa6dec8 (update vanilla and nextjs forms)
                     id="email"
                     name="email"
                     onChange={handleEmailChange}
@@ -87,11 +79,7 @@ export default function Form() {
                     Password:
                 </label>
                 <input
-<<<<<<< HEAD
                     className="form-field"
-=======
-                    className="form__field"
->>>>>>> fa6dec8 (update vanilla and nextjs forms)
                     id="password"
                     name="password"
                     onChange={handlePasswordChange}
@@ -101,13 +89,13 @@ export default function Form() {
                     Confirm Password:
                 </label>
                 <input
-                    className="form__field"
+                    className="form-field"
                     id="passwordConfirmation"
                     name="passwordConfirmation"
                     onChange={handlePasswordConfirmationChange}
                     type="password" />
 
-                <div className="form__controls">
+                <div className="form-controls">
                     <PieSwitch
                         label="Approve settings"
                         id="approveSettings"
@@ -124,9 +112,9 @@ export default function Form() {
                         onChange={handleNotificationsChange}
                     />
                 </div>
-                <div className='form__btns'>
-                    <PieButton className="form__btn" variant="secondary" type="reset">Reset</PieButton>
-                    <PieButton className="form__btn" type="submit">Submit</PieButton>
+                <div className='form-btns'>
+                    <PieButton className="form-btn" variant="secondary" type="reset">Reset</PieButton>
+                    <PieButton className="form-btn" type="submit">Submit</PieButton>
                 </div>
             </form>
             {formDataDisplay && (
