685d9f57d0334fcd3e518760b3c0e02fb20998af
Ben Siggery <bensiggery28@gmail.com>
2024-02-15 16:37:31 +0000

    chore: DSW-0 correct assistive text typo

--- nuxt-app/pages/pie-assistive-text.vue
+++ nuxt-app/pages/pie-assistive-text.vue
@@ -7,7 +7,7 @@
 
     <pie-assistive-text>Assistive Text Default</pie-assistive-text>
     <pie-assistive-text variant="success">Assistive Text Success</pie-assistive-text>
-    <pie-assistive-text variant="error">Assistive Text Success</pie-assistive-text>
+    <pie-assistive-text variant="error">Assistive Text Error</pie-assistive-text>
 
     <pie-divider></pie-divider>
 </template>
