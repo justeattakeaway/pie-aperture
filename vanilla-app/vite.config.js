1b5d8fba5a6f741ebdf7e19b083c5723edc70836
Ashley Watson-Nolan <nolly00@gmail.com>
2024-03-05 11:30:47 +0000

    feat: DSW-0 add basic templating to vanilla app (#84)

--- vanilla-app/vite.config.js
+++ vanilla-app/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import { join, resolve } from 'path';
 import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   build: {
-    outDir: path.join(__dirname, "dist"),
+    outDir: join(__dirname, "dist"),
     rollupOptions: {
       input: [
-        ...glob.sync(path.resolve(__dirname, 'index.html')),
-        ...glob.sync(path.resolve(__dirname, 'components', '*.html')),
-        ...glob.sync(path.resolve(__dirname, 'integrations', '*.html'))
+        ...glob.sync(resolve(__dirname, 'index.html')),
+        ...glob.sync(resolve(__dirname, 'components', '*.html')),
+        ...glob.sync(resolve(__dirname, 'integrations', '*.html'))
       ],
     },
   },
+  plugins: [injectHTML()],
 })
