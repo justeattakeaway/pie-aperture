c63f1f4959e91d02e1388b658e4e1ffca9f46694
Ben Siggery <bensiggery28@gmail.com>
2024-03-12 14:54:58 +0000

    chore: DSW-0 remove icon-button import

--- nextjs-app/src/pages/components/button.tsx
+++ nextjs-app/src/pages/components/button.tsx
@@ -1,5 +1,4 @@
 import { PieButton } from "@justeattakeaway/pie-button/dist/react";
-import { IconSearch } from "@justeattakeaway/pie-icons-webc/dist/react/IconSearch";
 import { useState } from "react";
 
 export default function Button() {
@@ -8,11 +7,6 @@ export default function Button() {
     return (
         <>
         <PieButton onClick={() => setCounter(counter + 1)}>Counter: {counter}</PieButton>
-        <h2>PIE Button - with Icon and text</h2>
-        <PieButton>
-            <IconSearch slot="icon"></IconSearch>
-            Search
-        </PieButton>
         </>
     );
 }
