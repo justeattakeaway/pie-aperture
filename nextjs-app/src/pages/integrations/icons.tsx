d9761856f92a6504134a2bb6542ad1b6625cc996
personal <jamieolivermaguire@googlemail.com>
2024-04-24 16:52:59 +0100

    restyle icons pages

--- nextjs-app/src/pages/integrations/icons.tsx
+++ nextjs-app/src/pages/integrations/icons.tsx
@@ -4,7 +4,14 @@ import * as icons from '@justeattakeaway/pie-icons-webc/dist/react';
 export default function Icons() {
     return (
         <NavigationLayout title="Icons Test Page">
-            <div style={{ display: 'flex', flexWrap: 'wrap', marginTop: 'var(--dt-spacing-c)' }}>
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(auto-fill, minmax(40px, 1fr))',
+                gap: 'var(--dt-spacing-c)',
+                justifyItems: 'center',
+                alignItems: 'center',
+                marginTop: 'var(--dt-spacing-c)'
+            }}>
                 {Object.entries(icons).map(([iconName, IconComponent]) => (
                     <IconComponent key={iconName} />
                 ))}
