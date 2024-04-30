2f79f2b5ea98d73a8d25fe5dd8cee2f22e5a22eb
personal <jamieolivermaguire@googlemail.com>
2024-04-30 11:24:05 +0100

    restyle icons pages to a grid

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "@justeattakeaway/pie-css";
 import "@/styles/main.scss";
+import "@/styles/icons.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
