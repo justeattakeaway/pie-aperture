07dc371452a01da6b9140f9f18df275fe4c33ff7
Josh Ng <joshua.ng2332@gmail.com>
2024-05-22 16:31:50 +0100

    feat: DSW-1994 Added NextJS 14 app (#140)
    * feat: DSW-1994 added Next 14 app
    
    * chore: DSW-1994 updated Yarn.lock

---
+++ nextjs-app-v14/src/app/components/spinner/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import NavigationLayout from "@/layout/navigation";
+import { PieSpinner } from '@justeattakeaway/pie-webc/react/spinner.js';
+
+export default function Spinner() {
+    return (
+        <NavigationLayout title="Spinner">
+        <PieSpinner/>
+        </NavigationLayout>
+    );
+}
