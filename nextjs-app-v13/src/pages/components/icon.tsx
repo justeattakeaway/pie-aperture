1b3c80870f68d5f0b3a8c01b9a52bfcca49cfae1
personal <jamieolivermaguire@googlemail.com>
2024-06-19 13:44:24 +0100

    remove .js from next 13 icon page

--- nextjs-app-v13/src/pages/components/icon.tsx
+++ nextjs-app-v13/src/pages/components/icon.tsx
@@ -1,6 +1,6 @@
 import NavigationLayout from "@/layout/navigation";
-import { IconPhone } from '@justeattakeaway/pie-icons-webc/dist/react/IconPhone.js';
-import { IconPhoneLarge } from '@justeattakeaway/pie-icons-webc/dist/react/IconPhoneLarge.js';
+import { IconPhone } from '@justeattakeaway/pie-icons-webc/dist/react/IconPhone';
+import { IconPhoneLarge } from '@justeattakeaway/pie-icons-webc/dist/react/IconPhoneLarge';
 
 export default function Icon() {
     return (
