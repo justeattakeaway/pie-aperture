e7a46cf9b7731e3243a891d4f87077fa15c95eaa
leksaBoiko <oleksandra.boiko@justeattakeaway.com>
2024-07-02 17:49:39 +0300

    Update Vanilla chip component

--- vanilla-app/js/chip.js
+++ vanilla-app/js/chip.js
@@ -3,10 +3,10 @@ import './utils/navigation.js';
 import './shared.js';
 
 document.querySelector('#app').innerHTML = `
-    <pie-chip>PIE Chip Default</pie-chip>
+    <pie-chip>PIE Chip</pie-chip>
 
     <pie-divider></pie-divider>
-    <pie-chip variant="outline">PIE Chip Outline</pie-chip>
+    <pie-chip variant="outline">PIE Chip</pie-chip>
 
     <pie-divider></pie-divider>
-    <pie-chip variant="ghost">PIE Chip Ghost</pie-chip>`;
+    <pie-chip variant="ghost">PIE Chip</pie-chip>`;
