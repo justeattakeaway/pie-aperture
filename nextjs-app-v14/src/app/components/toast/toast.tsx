b7cabff01e44db091753496cc12bd7c5d79b14e4
Raouf <raouf.sawehli@justeattakeaway.com>
2024-11-29 17:32:54 +0100

    feat(pie-toast-provider): DSW-2222 update prop values

--- nextjs-app-v14/src/app/components/toast/toast.tsx
+++ nextjs-app-v14/src/app/components/toast/toast.tsx
@@ -5,13 +5,9 @@ import { PieToast } from '@justeattakeaway/pie-webc/react/toast.js';
 
 export default function Toast() {
 
-   const leadingAction = {
-    text: 'Undo'
-   }
-
     return (
         <NavigationLayout title="Toast">
-            <PieToast message="This is a message" isDismissible leadingAction={leadingAction} />
+            <PieToast message="This is a message" isDismissible />
         </NavigationLayout>
     );
 }
