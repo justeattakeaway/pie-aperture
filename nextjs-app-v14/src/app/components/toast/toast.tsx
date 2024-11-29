727ead7ce868d862635d29747509ff0e7b409d06
Raouf <raouf.sawehli@justeattakeaway.com>
2024-11-29 17:23:14 +0100

    feat(pie-toast-provider): DSW-2222 add toast provider to apps

---
+++ nextjs-app-v14/src/app/components/toast/toast.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import NavigationLayout from "@/app/layout/navigation";
+import { PieToast } from '@justeattakeaway/pie-webc/react/toast.js';
+
+export default function Toast() {
+
+   const leadingAction = {
+    text: 'Undo'
+   }
+
+    return (
+        <NavigationLayout title="Toast">
+            <PieToast duration={null} message="This is a message" isDismissible leadingAction={leadingAction} />
+        </NavigationLayout>
+    );
+}
