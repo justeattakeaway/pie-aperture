86cff06d18fc2bf975883a526c3fd9739291a3d2
Ashley Watson-Nolan <ashley.watson-nolan@cd2qgxd4hp.acttoday.com>
2025-08-18 13:41:15 +0100

    feat(pie-data-table): DSW-3271 add the data-table component

---
+++ vanilla-app/js/data-table.js
@@ -0,0 +1,27 @@
+import '@justeattakeaway/pie-webc/components/data-table.js';
+import '@justeattakeaway/pie-webc/components/data-table-header.js';
+import '@justeattakeaway/pie-webc/components/button.js';
+import { sampleColumns, sampleData } from '../data/sampleTableData.js';
+import './utils/navigation.js';
+import './shared.js';
+
+document.querySelector('#app').innerHTML = `
+<pie-data-table data-test-id="pie-data-table"></pie-data-table>
+
+<pie-divider></pie-divider>
+
+
+<pie-data-table columns={sampleColumns} data={sampleData} data-test-id="PieDataTable">
+    <pie-data-table-header slot="table-header" title="PIE Data Table – Table with header" subtitle="Optional subtitle text">
+        <pie-button slot="action-button" variant="secondary" size="xsmall">Action</pie-button>
+        <pie-button slot="action-button" variant="secondary" size="xsmall">Action</pie-button>
+        <pie-button slot="action-button" variant="secondary" size="xsmall">Action</pie-button>
+    </pie-data-table-header>
+</pie-data-table>
+`;
+
+const dataTables = document.querySelectorAll('pie-data-table');
+dataTables.forEach(dt => {
+    dt.columns = sampleColumns;
+    dt.data = sampleData;
+});
