b2eb5b2c338f4690db73f4357c158293827e86d5
Ashley Watson-Nolan <ashley.watson-nolan@cd2qgxd4hp.acttoday.com>
2025-08-18 13:41:15 +0100

    feat(pie-data-table): DSW-3271 add the data-table component

--- nextjs-app-v14/src/app/components/home-page.tsx
+++ nextjs-app-v14/src/app/components/home-page.tsx
@@ -12,40 +12,41 @@ export default function HomePage() {
     <NavigationLayout title="Aperture">
     <h2>Demo Integrations</h2>
     <ul>
-      <li><PieLink onClick={() => router.push('/integrations/controlled-form')} tag="button">Controlled Form Demo</PieLink></li>
-      <li><PieLink onClick={() => router.push('/integrations/uncontrolled-form')} tag="button">Uncontrolled Form Demo</PieLink></li>
+        <li><PieLink onClick={() => router.push('/integrations/controlled-form')} tag="button">Controlled Form Demo</PieLink></li>
+        <li><PieLink onClick={() => router.push('/integrations/uncontrolled-form')} tag="button">Uncontrolled Form Demo</PieLink></li>
     </ul>
     <h2>Component Pages</h2>
     <ul>
-    <li><PieLink onClick={() => router.push('/components/assistive-text')} tag="button">Assistive Text</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/avatar')} tag="button">Avatar</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/button')} tag="button">Button</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/breadcrumb')} tag="button">Breadcrumb</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/card')} tag="button">Card</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/checkbox')} tag="button">Checkbox</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/checkbox-group')} tag="button">Checkbox Group</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/chip')} tag="button">Chip</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/cookie-banner')} tag="button">Cookie Banner</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/custom-tag')} tag="button">Custom Tag</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/divider')} tag="button">Divider</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/form-label')} tag="button">Form Label</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/icon')} tag="button">Icon</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/icon-button')} tag="button">Icon Button</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/link')} tag="button">Link</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/lottie-player')} tag="button">Lottie Player</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/modal')} tag="button">Modal</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/notification')} tag="button">Notification</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/radio')} tag="button">Radio</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/radio-group')} tag="button">Radio Group</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/spinner')} tag="button">Spinner</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/switch')} tag="button">Switch</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/select')} tag="button">Select</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/tag')} tag="button">Tag</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/text-input')} tag="button">Text Input</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/textarea')} tag="button">Textarea</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/toast')} tag="button">Toast</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/toast-provider')} tag="button">Toast Provider</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/thumbnail')} tag="button">Thumbnail</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/assistive-text')} tag="button">Assistive Text</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/avatar')} tag="button">Avatar</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/button')} tag="button">Button</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/breadcrumb')} tag="button">Breadcrumb</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/card')} tag="button">Card</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/checkbox')} tag="button">Checkbox</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/checkbox-group')} tag="button">Checkbox Group</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/chip')} tag="button">Chip</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/cookie-banner')} tag="button">Cookie Banner</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/custom-tag')} tag="button">Custom Tag</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/data-table')} tag="button">Data Table</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/divider')} tag="button">Divider</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/form-label')} tag="button">Form Label</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/icon')} tag="button">Icon</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/icon-button')} tag="button">Icon Button</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/link')} tag="button">Link</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/lottie-player')} tag="button">Lottie Player</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/modal')} tag="button">Modal</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/notification')} tag="button">Notification</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/radio')} tag="button">Radio</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/radio-group')} tag="button">Radio Group</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/spinner')} tag="button">Spinner</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/switch')} tag="button">Switch</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/select')} tag="button">Select</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/tag')} tag="button">Tag</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/text-input')} tag="button">Text Input</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/textarea')} tag="button">Textarea</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/toast')} tag="button">Toast</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/toast-provider')} tag="button">Toast Provider</PieLink></li>
+        <li><PieLink onClick={() => router.push('/components/thumbnail')} tag="button">Thumbnail</PieLink></li>
     </ul>
     </NavigationLayout>
   );
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -7,34 +7,35 @@
 		<h3>Component Pages</h3>
 		<ul>
 			<li><pie-link href="/components/assistive-text">Assistive Text</pie-link></li>
-      <li><pie-link href="/components/avatar">Avatar</pie-link></li>
+            <li><pie-link href="/components/avatar">Avatar</pie-link></li>
 			<li><pie-link href="/components/button">Button</pie-link></li>
-      <li><pie-link href="/components/breadcrumb">Breadcrumb</pie-link></li>
+            <li><pie-link href="/components/breadcrumb">Breadcrumb</pie-link></li>
 			<li><pie-link href="/components/card">Card</pie-link></li>
-      <li><pie-link href="/components/checkbox">Checkbox</pie-link></li>
-      <li><pie-link href="/components/checkbox-group">Checkbox Group</pie-link></li>
-      <li><pie-link href="/components/chip">Chip</pie-link></li>
-      <li><pie-link href="/components/cookie-banner">Cookie Banner</pie-link></li>
-      <li><pie-link href="/components/custom-tag">Custom Tag</pie-link></li>
-      <li><pie-link href="/components/divider">Divider</pie-link></li>
-      <li><pie-link href="/components/form-label">Form Label</pie-link></li>
-      <li><pie-link href="/components/icon">Icon</pie-link></li>
-      <li><pie-link href="/components/icon-button">Icon Button</pie-link></li>
-      <li><pie-link href="/components/lottie-player">Lottie Player</pie-link></li>
-      <li><pie-link href="/components/link">Link</pie-link></li>
-      <li><pie-link href="/components/modal">Modal</pie-link></li>
-      <li><pie-link href="/components/notification">Notification</pie-link></li>
-      <li><pie-link href="/components/radio">Radio</pie-link></li>
-      <li><pie-link href="/components/radio-group">Radio Group</pie-link></li>
-      <li><pie-link href="/components/spinner">Spinner</pie-link></li>
-      <li><pie-link href="/components/switch">Switch</pie-link></li>
-      <li><pie-link href="/components/select">Select</pie-link></li>
-      <li><pie-link href="/components/tag">Tag</pie-link></li>
-      <li><pie-link href="/components/text-input">Text Input</pie-link></li>
-      <li><pie-link href="/components/textarea">Textarea</pie-link></li>
-      <li><pie-link href="/components/toast">Toast</pie-link></li>
-      <li><pie-link href="/components/toast-provider">Toast Provider</pie-link></li>
-      <li><pie-link href="/components/thumbnail">Thumbnail</pie-link></li>
+            <li><pie-link href="/components/checkbox">Checkbox</pie-link></li>
+            <li><pie-link href="/components/checkbox-group">Checkbox Group</pie-link></li>
+            <li><pie-link href="/components/chip">Chip</pie-link></li>
+            <li><pie-link href="/components/cookie-banner">Cookie Banner</pie-link></li>
+            <li><pie-link href="/components/custom-tag">Custom Tag</pie-link></li>
+            <li><pie-link href="/components/data-table">Data Table</pie-link></li>
+            <li><pie-link href="/components/divider">Divider</pie-link></li>
+            <li><pie-link href="/components/form-label">Form Label</pie-link></li>
+            <li><pie-link href="/components/icon">Icon</pie-link></li>
+            <li><pie-link href="/components/icon-button">Icon Button</pie-link></li>
+            <li><pie-link href="/components/lottie-player">Lottie Player</pie-link></li>
+            <li><pie-link href="/components/link">Link</pie-link></li>
+            <li><pie-link href="/components/modal">Modal</pie-link></li>
+            <li><pie-link href="/components/notification">Notification</pie-link></li>
+            <li><pie-link href="/components/radio">Radio</pie-link></li>
+            <li><pie-link href="/components/radio-group">Radio Group</pie-link></li>
+            <li><pie-link href="/components/spinner">Spinner</pie-link></li>
+            <li><pie-link href="/components/switch">Switch</pie-link></li>
+            <li><pie-link href="/components/select">Select</pie-link></li>
+            <li><pie-link href="/components/tag">Tag</pie-link></li>
+            <li><pie-link href="/components/text-input">Text Input</pie-link></li>
+            <li><pie-link href="/components/textarea">Textarea</pie-link></li>
+            <li><pie-link href="/components/toast">Toast</pie-link></li>
+            <li><pie-link href="/components/toast-provider">Toast Provider</pie-link></li>
+            <li><pie-link href="/components/thumbnail">Thumbnail</pie-link></li>
 		</ul>
 	</div>
 </template>
