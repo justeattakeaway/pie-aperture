ef4318f7fb02fe70fc9f806464976c43279bed86
Katarina Neskovic <katarina.neskovic@justeattakeaway.com>
2025-08-26 11:12:45 +0200

    update avatar.js

--- vanilla-app/js/avatar.js
+++ vanilla-app/js/avatar.js
@@ -6,4 +6,6 @@ document.querySelector('#app').innerHTML = `
     <h1>Avatar Static Initials</h1>
     <pie-avatar label="Ada Lovelace" tag="div"></pie-avatar>
     <h1>Avatar Fallback</h1>
+    <h2>Avatar Test</h2>
     <pie-avatar tag="div"></pie-avatar>`;
+
