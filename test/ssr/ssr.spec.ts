55fb45fe8cd8e5fcfa46ec72e534ed6f529e8be4
personal <jamieolivermaguire@googlemail.com>
2025-09-26 13:55:32 +0100

    vanilla and ssr tests

--- test/ssr/ssr.spec.ts
+++ test/ssr/ssr.spec.ts
@@ -19,6 +19,7 @@ const components = [
     'checkbox-group',
     'chip',
     'cookie-banner',
+    'custom-tag',
     'divider',
     'form-label',
     'icon',
--- vanilla-app/js/tag.js
+++ vanilla-app/js/tag.js
@@ -1,7 +1,6 @@
 import '@justeattakeaway/pie-webc/components/tag.js';
 import { variants } from '@justeattakeaway/pie-webc/components/tag.js';
 import '@justeattakeaway/pie-webc/components/divider.js';
-import '@justeattakeaway/pie-icons-webc/dist/IconOfferFilled.js';
 
 import './shared.js';
 import './utils/navigation.js';
@@ -58,13 +57,5 @@ document.querySelector('#app').innerHTML = `
                 ${variant}
             </pie-tag>
         `).join('')}
-    </div>
-
-    <h3>Customised Tag using CSS parts</h3>
-    <div class="c-tag-container c-tag-container--dark">
-        <pie-tag class="custom-style custom-1">
-            <icon-offer-filled slot="icon"></icon-offer-filled>
-            <span>Label 1</span>
-        </pie-tag>
     </div>
 </div>`;
