a4ee5183e33e698a75a9c506fcb52b7c5cb1bcab
personal <jamieolivermaguire@googlemail.com>
2025-09-26 12:03:00 +0100

    vanilla

--- vanilla-app/js/tag.js
+++ vanilla-app/js/tag.js
@@ -1,6 +1,8 @@
 import '@justeattakeaway/pie-webc/components/tag.js';
 import { variants } from '@justeattakeaway/pie-webc/components/tag.js';
 import '@justeattakeaway/pie-webc/components/divider.js';
+import '@justeattakeaway/pie-icons-webc/dist/IconOfferFilled.js';
+
 import './shared.js';
 import './utils/navigation.js';
 import '../css/tag.css';
@@ -56,4 +58,13 @@ document.querySelector('#app').innerHTML = `
                 ${variant}
             </pie-tag>
         `).join('')}
-    </div>`;
+    </div>
+
+    <h3>Customised Tag using CSS parts</h3>
+    <div class="c-tag-container c-tag-container--dark">
+        <pie-tag class="custom-style custom-1">
+            <icon-offer-filled slot="icon"></icon-offer-filled>
+            <span>Label 1</span>
+        </pie-tag>
+    </div>
+</div>`;
