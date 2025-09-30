601c21c863438ed444759ad33e366f77a6e96da3
Josh Ng <joshua.ng2332@gmail.com>
2025-09-30 16:25:59 +0100

    feat: DSW-3178 added Divider, Form Label and Icon pages

--- nextjs-app-v15/src/app/components/home-page.tsx
+++ nextjs-app-v15/src/app/components/home-page.tsx
@@ -27,10 +27,10 @@ export default function HomePage() {
     <li><PieLink onClick={() => router.push('/components/checkbox-group')} tag="button">Checkbox Group</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/chip')} tag="button">Chip</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/cookie-banner')} tag="button">Cookie Banner</PieLink></li>
-    {/* <li><PieLink onClick={() => router.push('/components/divider')} tag="button">Divider</PieLink></li>
+    <li><PieLink onClick={() => router.push('/components/divider')} tag="button">Divider</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/form-label')} tag="button">Form Label</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/icon')} tag="button">Icon</PieLink></li>
-    <li><PieLink onClick={() => router.push('/components/icon-button')} tag="button">Icon Button</PieLink></li>
+    {/* <li><PieLink onClick={() => router.push('/components/icon-button')} tag="button">Icon Button</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/link')} tag="button">Link</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/lottie-player')} tag="button">Lottie Player</PieLink></li>
     <li><PieLink onClick={() => router.push('/components/modal')} tag="button">Modal</PieLink></li>
