a39462a73979291814bab1c0195945efe673dbaf
Josh Ng <joshua.ng2332@gmail.com>
2025-10-07 17:07:11 +0100

    feat: DSW-3178 added Textarea, Thumbnail and Toast pages (#354)

---
+++ nextjs-app-v15/src/app/components/textarea/textarea.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import NavigationLayout from "@/app/layout/navigation";
+import { PieTextarea } from '@justeattakeaway/pie-webc/react/textarea.js';
+import { PieDivider } from '@justeattakeaway/pie-webc/react/divider.js';
+
+export default function Textarea() {
+
+    return (
+        <NavigationLayout title="Textarea">
+            <PieTextarea value="foo"/>
+
+            <PieDivider />
+
+            <PieTextarea
+                value="foo"
+                resize="manual"
+            />
+        </NavigationLayout>
+    );
+}
