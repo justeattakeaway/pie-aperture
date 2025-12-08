89679eace201d73018991726fff7007e99511861
maledr5 <maledr5@hotmail.es>
2025-12-08 09:51:21 +0100

    DSW-3569 reproduce overflow issue

--- nextjs-app-v15/src/app/components/modal/modal.tsx
+++ nextjs-app-v15/src/app/components/modal/modal.tsx
@@ -4,6 +4,8 @@ import NavigationLayout from "@/app/layout/navigation";
 import { PieButton } from '@justeattakeaway/pie-webc/react/button.js';
 import { PieModal } from '@justeattakeaway/pie-webc/react/modal.js';
 import { useState } from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Modal() {
     const [isModalOpen, setIsModalOpen] = useState(true);
@@ -12,6 +14,8 @@ export default function Modal() {
         <NavigationLayout title="Modal">
         <PieButton onClick={() => setIsModalOpen(true)}>Open Modal</PieButton>
         <PieModal
+            className="modal-overflow-visible"
+            style={{ overflow: 'visible' }}
             isOpen={isModalOpen}
             heading="Modal Header"
             isDismissible
@@ -22,6 +26,9 @@ export default function Modal() {
             onPieModalBack={() => setIsModalOpen(false)}
         >
             <p>Modal content</p>
+            <div style={{ position: "relative" }}>
+                <ReactDatePicker popperPlacement="top" monthsShown={2} />
+            </div>
         </PieModal>
         </NavigationLayout>
     );
