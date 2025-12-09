2986cfe7a3a8f0ae7f190947bb05cdcde648e9bf
personal <jamieolivermaguire@googlemail.com>
2025-12-09 17:45:23 +0000

    typography page vanilla

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -16,33 +16,33 @@ import '@justeattakeaway/pie-webc/components/divider.js';
     height: 100%;
 
     @font-face {
-      font-family: JETSansDigital;
-      src: url('https://d30v2pzvrfyzpo.cloudfront.net/fonts/JETSansDigital-Regular-optimised.woff2') format("woff2"),
-        url('https://d30v2pzvrfyzpo.cloudfront.net/fonts/JETSansDigital-Regular-optimised.woff') format("woff");
-      font-weight: 400;
-      font-display: swap;
+        font-family: JETSansDigital;
+        src: url('https://pie-design-system-cdn.production.jet-external.com/fonts/JETSansDigital-VF-optimised-extended.woff2') format("woff2");
+        font-weight: 100 1000;
+        font-stretch: 25% 151%;
+        font-style: oblique 0deg 20deg;
+        font-display: swap;
     }
 
     @font-face {
-      font-family: JETSansDigital;
-      src: url('https://d30v2pzvrfyzpo.cloudfront.net/fonts/JETSansDigital-Bold-optimised.woff2') format("woff2"),
-        url('https://d30v2pzvrfyzpo.cloudfront.net/fonts/JETSansDigital-Bold-optimised.woff') format("woff");
-      font-weight: 700;
-      font-display: swap;
+        font-family: PTMono;
+        src: url('https://d30v2pzvrfyzpo.cloudfront.net/fonts/PTMono-Regular.ttf') format("truetype");
+        font-weight: 400;
+        font-display: swap;
     }
-  }
+}
 
-  body {
+body {
     font-family: JetSansDigital;
     margin: auto;
     min-height: 100%;
     padding: 16px;
     max-width: 720px;
     color: var(--dt-color-content-default);
-  }
+}
 
-  pie-divider {
+pie-divider {
     display: block;
     margin-block: 2em;
-  }
+}
 </style>
