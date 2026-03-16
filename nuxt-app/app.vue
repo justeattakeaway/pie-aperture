ba14acaf1df6516f9263ce3998bff61f7e75edfa
Raouf <abdulraoufswehli58@gmail.com>
2026-03-16 11:38:26 +0300

    feat: update apps to latest pie and JET font file (#410)
    * update to latest pie and font
    
    * update to latest pie and font
    
    * fix buil

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -11,38 +11,32 @@ import '@justeattakeaway/pie-webc/components/divider.js';
 </script>
 
 <style lang="scss">
+  @font-face {
+    font-family: JETSansDigital;
+    src: url('https://pie-design-system-cdn.production.jet-external.com/fonts/JETSansDigital-VF-opt.woff2') format("woff2");
+    font-weight: 100 1000;
+    font-stretch: 25% 151%;
+    font-style: oblique 0deg 20deg;
+    font-display: swap;
+  }
+
   html {
     background-color: var(--dt-color-container-default);
     height: 100%;
+  }
 
-    @font-face {
-        font-family: JETSansDigital;
-        src: url('https://pie-design-system-cdn.production.jet-external.com/fonts/JETSansDigital-VF-optimised-extended.woff2') format("woff2");
-        font-weight: 100 1000;
-        font-stretch: 25% 151%;
-        font-style: oblique 0deg 20deg;
-        font-display: swap;
-    }
-
-    @font-face {
-        font-family: PTMono;
-        src: url('https://d30v2pzvrfyzpo.cloudfront.net/fonts/PTMono-Regular.ttf') format("truetype");
-        font-weight: 400;
-        font-display: swap;
-    }
-}
-
-body {
-    font-family: JetSansDigital;
+  body {
+    font-family: JETSansDigital;
+    font-feature-settings: "tnum";
     margin: auto;
     min-height: 100%;
     padding: 16px;
     max-width: 720px;
     color: var(--dt-color-content-default);
-}
+  }
 
-pie-divider {
+  pie-divider {
     display: block;
     margin-block: 2em;
-}
+  }
 </style>
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -2,6 +2,20 @@
 export default defineNuxtConfig({
   ssr: true,
 
+  app: {
+      head: {
+          link: [
+              {
+                  rel: 'preload',
+                  href: 'https://pie-design-system-cdn.production.jet-external.com/fonts/JETSansDigital-VF-opt.woff2',
+                  as: 'font',
+                  type: 'font/woff2',
+                  crossorigin: 'anonymous',
+              },
+          ],
+      },
+  },
+
   nitro: {
       moduleSideEffects: [
           '@justeattakeaway/pie-icons-webc',
