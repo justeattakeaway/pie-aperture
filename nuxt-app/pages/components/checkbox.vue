073512db969f582e116d366780928f61c99a2dc8
Jamie Maguire <jamieolivermaguire@googlemail.com>
2026-05-14 10:14:04 +0100

    DSW-3913: checkbox label props (#474)
    * DSW-3913: checkbox label props
    
    * update version
    
    * add checked examples

--- nuxt-app/pages/components/checkbox.vue
+++ nuxt-app/pages/components/checkbox.vue
@@ -1,7 +1,50 @@
 <template>
   <div>
-    <pie-checkbox :checked="checked" @change="handleCheckboxChange">
-      {{ `checked: ${checked}` }}
+    <h2>Default (trailing, hug)</h2>
+    <pie-checkbox :checked="checked1" @change="() => checked1 = !checked1">
+      {{ `checked: ${checked1}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="hug"</h2>
+    <pie-checkbox :checked="checked2" @change="() => checked2 = !checked2" labelPosition="leading">
+      {{ `checked: ${checked2}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="fill"</h2>
+    <pie-checkbox :checked="checked3" @change="() => checked3 = !checked3" labelPosition="leading" labelFit="fill">
+      {{ `checked: ${checked3}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>labelPosition="trailing", labelFit="fill"</h2>
+    <pie-checkbox :checked="checked4" @change="() => checked4 = !checked4" labelFit="fill">
+      {{ `checked: ${checked4}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>Default (trailing, hug) - Checked</h2>
+    <pie-checkbox :checked="checked5" @change="() => checked5 = !checked5">
+      {{ `checked: ${checked5}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="hug" - Checked</h2>
+    <pie-checkbox :checked="checked6" @change="() => checked6 = !checked6" labelPosition="leading">
+      {{ `checked: ${checked6}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="fill" - Checked</h2>
+    <pie-checkbox :checked="checked7" @change="() => checked7 = !checked7" labelPosition="leading" labelFit="fill">
+      {{ `checked: ${checked7}` }}
+    </pie-checkbox>
+
+    <br />
+    <h2>labelPosition="trailing", labelFit="fill" - Checked</h2>
+    <pie-checkbox :checked="checked8" @change="() => checked8 = !checked8" labelFit="fill">
+      {{ `checked: ${checked8}` }}
     </pie-checkbox>
   </div>
 </template>
@@ -15,10 +58,13 @@ definePageMeta({
     title: 'Checkbox',
 });
 
-let checked = ref(false);
-
-function handleCheckboxChange() {
-  checked.value = !checked.value;
-}
+let checked1 = ref(false);
+let checked2 = ref(false);
+let checked3 = ref(false);
+let checked4 = ref(false);
+let checked5 = ref(true);
+let checked6 = ref(true);
+let checked7 = ref(true);
+let checked8 = ref(true);
 
 </script>
--- vanilla-app/js/checkbox.js
+++ vanilla-app/js/checkbox.js
@@ -2,20 +2,61 @@ import '@justeattakeaway/pie-webc/components/checkbox.js';
 import './shared.js';
 import './utils/navigation.js';
 
-let checked = false;
+const state = {
+    cb1: false,
+    cb2: false,
+    cb3: false,
+    cb4: false,
+    cb5: true,
+    cb6: true,
+    cb7: true,
+    cb8: true,
+};
 
-function handleCheckboxChange() {
-    checked = !checked;
-
-    const pieCheckbox = document.querySelector('pie-checkbox');
-    pieCheckbox.innerHTML = `checked: ${checked}`;
-    pieCheckbox.checked = checked;
+function createHandler(key) {
+    return () => {
+        state[key] = !state[key];
+        const el = document.querySelector(`#${key}`);
+        el.innerHTML = `checked: ${state[key]}`;
+        el.checked = state[key];
+    };
 }
 
 // Set initial HTML structure
 document.querySelector('#app').innerHTML = `
-    <pie-checkbox>checked: ${checked}</pie-checkbox>
+    <h2>Default (trailing, hug)</h2>
+    <pie-checkbox id="cb1">checked: ${state.cb1}</pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="hug"</h2>
+    <pie-checkbox id="cb2" labelPosition="leading">checked: ${state.cb2}</pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="fill"</h2>
+    <pie-checkbox id="cb3" labelPosition="leading" labelFit="fill">checked: ${state.cb3}</pie-checkbox>
+
+    <br />
+    <h2>labelPosition="trailing", labelFit="fill"</h2>
+    <pie-checkbox id="cb4" labelFit="fill">checked: ${state.cb4}</pie-checkbox>
+
+    <br />
+    <h2>Default (trailing, hug) - Checked</h2>
+    <pie-checkbox id="cb5" checked>checked: ${state.cb5}</pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="hug" - Checked</h2>
+    <pie-checkbox id="cb6" checked labelPosition="leading">checked: ${state.cb6}</pie-checkbox>
+
+    <br />
+    <h2>labelPosition="leading", labelFit="fill" - Checked</h2>
+    <pie-checkbox id="cb7" checked labelPosition="leading" labelFit="fill">checked: ${state.cb7}</pie-checkbox>
+
+    <br />
+    <h2>labelPosition="trailing", labelFit="fill" - Checked</h2>
+    <pie-checkbox id="cb8" checked labelFit="fill">checked: ${state.cb8}</pie-checkbox>
 `;
 
-// Add event listener to pie-checkbox for change events
-document.querySelector('pie-checkbox').addEventListener('change', handleCheckboxChange);
+// Add individual event listeners
+Object.keys(state).forEach((key) => {
+    document.querySelector(`#${key}`).addEventListener('change', createHandler(key));
+});
