572627ee895ff7c266ae33128ac365d438984934
personal <jamieolivermaguire@googlemail.com>
2026-07-29 12:31:13 +0100

    sync

--- nuxt-app/pages/components/list-item-checkbox-selection.vue
+++ nuxt-app/pages/components/list-item-checkbox-selection.vue
@@ -3,16 +3,16 @@
         <h2 id="checkbox-leading-heading" style="padding: 8px 0;">Checkbox group - leading control (Pepperoni pre-selected)</h2>
         <pie-checkbox-group @change="onLeadingChange">
             <pie-form-label slot="label">Selected toppings: {{ leadingSelected.join(', ') || 'none' }}</pie-form-label>
-            <pie-list-item selectiontype="checkbox" primaryText="Cheese" secondaryText="Extra mature" metaText="Free">
+            <pie-list-item selectionType="checkbox" primaryText="Cheese" secondaryText="Extra mature" metaText="Free">
                 <pie-checkbox slot="leading" name="cheese" value="cheese"></pie-checkbox>
             </pie-list-item>
-            <pie-list-item selectiontype="checkbox" primaryText="Pepperoni" secondaryText="Spicy">
+            <pie-list-item selectionType="checkbox" primaryText="Pepperoni" secondaryText="Spicy">
                 <pie-checkbox slot="leading" name="pepperoni" value="pepperoni" checked></pie-checkbox>
             </pie-list-item>
-            <pie-list-item selectiontype="checkbox" disabled primaryText="Mushrooms" secondaryText="Out of season">
+            <pie-list-item selectionType="checkbox" disabled primaryText="Mushrooms" secondaryText="Out of season">
                 <pie-checkbox slot="leading" name="mushrooms" value="mushrooms" disabled></pie-checkbox>
             </pie-list-item>
-            <pie-list-item selectiontype="checkbox" primaryText="Olives" metaText="£0.50">
+            <pie-list-item selectionType="checkbox" primaryText="Olives" metaText="£0.50">
                 <pie-checkbox slot="leading" name="olives" value="olives"></pie-checkbox>
             </pie-list-item>
         </pie-checkbox-group>
@@ -22,16 +22,16 @@
         <h2 id="checkbox-trailing-heading" style="padding: 8px 0;">Checkbox group - trailing control (Pepperoni pre-selected)</h2>
         <pie-checkbox-group @change="onTrailingChange">
             <pie-form-label slot="label">Selected toppings: {{ trailingSelected.join(', ') || 'none' }}</pie-form-label>
-            <pie-list-item selectiontype="checkbox" primaryText="Cheese" secondaryText="Extra mature">
+            <pie-list-item selectionType="checkbox" primaryText="Cheese" secondaryText="Extra mature">
                 <pie-checkbox slot="trailing" name="cheese" value="cheese"></pie-checkbox>
             </pie-list-item>
-            <pie-list-item selectiontype="checkbox" primaryText="Pepperoni" secondaryText="Spicy">
+            <pie-list-item selectionType="checkbox" primaryText="Pepperoni" secondaryText="Spicy">
                 <pie-checkbox slot="trailing" name="pepperoni" value="pepperoni" checked></pie-checkbox>
             </pie-list-item>
-            <pie-list-item selectiontype="checkbox" disabled primaryText="Mushrooms" secondaryText="Out of season">
+            <pie-list-item selectionType="checkbox" disabled primaryText="Mushrooms" secondaryText="Out of season">
                 <pie-checkbox slot="trailing" name="mushrooms" value="mushrooms" disabled></pie-checkbox>
             </pie-list-item>
-            <pie-list-item selectiontype="checkbox" primaryText="Olives" metaText="£0.50">
+            <pie-list-item selectionType="checkbox" primaryText="Olives" metaText="£0.50">
                 <pie-checkbox slot="trailing" name="olives" value="olives"></pie-checkbox>
             </pie-list-item>
         </pie-checkbox-group>
